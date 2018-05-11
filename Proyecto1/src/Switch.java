c9ad9d6bb1c21324bff4414182d3abb8aa77643c
cesfernandez1 <cesarfer9824@gmail.com>
2018-05-11 10:57:24 +0200

    Switch

---
+++ Proyecto1/src/Switch.java
@@ -0,0 +1,23 @@
+
+public class Switch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int numero = 20;
+		
+		switch(numero) {
+		
+		case 0: System.out.println("Soy cero");
+		
+		case 1: System.out.println("Soy uno");
+		
+		case 2: System.out.println("Soy dos");
+		
+		default: System.out.println("Soy otro");
+			break;
+		
+		}
+	}
+
+}
