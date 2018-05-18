24bfa0f81e5ee7247fe6aa6888c6bd0f3a6092f5
cesfernandez1 <cesarfer9824@gmail.com>
2018-05-18 11:22:52 +0200

    18/05/2018

---
+++ Proyecto1/src/Arrays.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String [] contactos = new String[6];
+		
+		int [] edad = {19,20,30,40,50,60};
+		
+		System.out.println("La edad seleccionada es : " +edad[4]);
+		
+		
+		for(int i=0; i<edad.length; i++) {
+			System.out.println(edad[i]);
+		}
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		for(int i=0; i<contactos.length; i++) {
+			System.out.println("Introduzca un contacto en la posicion: " +i);
+			contactos[i]=teclado.next();
+		}
+		
+		for(int i=0; i<contactos.length; i++) {
+			System.out.println(contactos[i]);
+		}
+		
+		
+
+	}
+
+}
--- Proyecto1/src/DoWhile2.java
+++ Proyecto1/src/DoWhile2.java
@@ -5,8 +5,11 @@ public class DoWhile2 {
 	public static void main(String[] args) {
 		int opcion = 0;
 		int num1 =0;
-		int num2 = 0;
+		int  num2 = 0;
+		double base = 0;
+		double exp = 0;
 		int resultado = 0;
+		double resultado2 = 0;
 		
 		Scanner teclado = new Scanner(System.in);
 		
@@ -19,7 +22,7 @@ public class DoWhile2 {
 			System.out.println("4.Dividir");
 			System.out.println("5.Potencia");
 			System.out.println("6.Raiz");
-			System.out.println("7.Maximo");
+			System.out.println("7.Maximo de dos numeros");
 			System.out.println("8.Minimo");
 			System.out.println("9.Salir");
 			
@@ -30,46 +33,80 @@ public class DoWhile2 {
 				num1 = teclado.nextInt();
 				System.out.println("Introduzca otro numero");
 				num2 = teclado.nextInt();
-				resultado = num1 + num2;
+				resultado = sumar(num1, num2);
 				System.out.println(" La suma es: " +resultado);
 				break;
 				
-			case 2: System.out.println("La resta de 1-1 son siete");
+			case 2: System.out.println("Introduzca un numero");
 				num1 = teclado.nextInt();
 				System.out.println("Introduzca otro numero");
 				num2 = teclado.nextInt();
-				resultado = num1 - num2;
+				resultado = restar(num1 , num2);
 				System.out.println(" La suma es: " +resultado);
 				break;
-			case 3: System.out.println("La multiplicacion de 1*1 son siete");
+			case 3: System.out.println("Introduzca un numero");
 				num1 = teclado.nextInt();
 				System.out.println("Introduzca otro numero");
 				num2 = teclado.nextInt();
-				resultado = num1 * num2;
+				resultado = multi(num1, num2);
 				System.out.println(" La suma es: " +resultado);
 				break;
-			case 4: System.out.println("La division de 1/1 son siete");
+			case 4: System.out.println("Introduzca un numero");
 				num1 = teclado.nextInt();
 				System.out.println("Introduzca otro numero");
 				num2 = teclado.nextInt();
-				resultado = num1 / num2;
+				resultado = divid(num1, num2);
 				System.out.println(" La suma es: " +resultado);
 				break;
-			case 5: System.out.println("La potencia de 1^1 son siete");
+			case 5: System.out.println("Introduzca un numero");
+				base = teclado.nextInt();
+				System.out.println("Introduzca otro numero");
+				exp = teclado.nextInt();
+				resultado2 = Math.pow(base,exp);
+				System.out.println(" La potencia de base " +base+ " exponente " +exp+ " es: " +resultado2);
 			break;
-			case 6: System.out.println("La raiz de 1 son siete");
+			case 6: System.out.println("Introduzca un numero");
+				base = teclado.nextInt();
+				resultado2 = Math.sqrt(base);
+				System.out.println(" La raiz cuadrada de " +base+ " es : " +resultado2);
+				break;
+			case 7: System.out.println("Introduzca un numero");
+			num1 = teclado.nextInt();
+			System.out.println("Introduzca otro numero");
+			num2 = teclado.nextInt();
+			resultado = Math.max(num1, num2);
+			System.out.println(" El maximo es: " +resultado);
 			break;
-			case 7: System.out.println("El maximo  de 1+1 son siete");
+			case 8: System.out.println("Introduzca un numero");
+			num1 = teclado.nextInt();
+			System.out.println("Introduzca otro numero");
+			num2 = teclado.nextInt();
+			resultado = Math.min(num1, num2);
+			System.out.println(" El minimo es : " +resultado);
 			break;
-			case 8: System.out.println("El minimo de 1+1 son siete");
-			break;
-			case 9: System.out.println("La suma de 1+1 son siete");
+			case 9: System.out.println("Saliste");
 			break;
 			default: System.out.println("Opcion no valida");
 			}
 			
 		}while(opcion >= 1);
 
+	}
+	
+	private static int sumar(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	private static int restar(int num1, int num2) {
+		return num1 - num2;
+	}
+	
+	private static int multi(int num1, int num2) {
+		return num1 * num2;
+	}
+	
+	private static int divid(int num1, int num2) {
+		return num1 / num2;
 	}
 
 }
